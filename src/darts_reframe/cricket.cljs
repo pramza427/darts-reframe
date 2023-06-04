11284ae19e8baa540cddc47e37a9f0c598544448
pramza427 <pramza427@gmail.com>
2023-06-04 01:54:52 -0600

    Improve Cricket UI

--- src/darts_reframe/cricket.cljs
+++ src/darts_reframe/cricket.cljs
@@ -2,7 +2,8 @@
   (:require [clojure.string :as str]
             [re-frame.core :as rf]))
 
-
+(def NUMBERS [15 16 17 18 19 20 25])
+(def EXTRA-NUMBERS [11 12 13 14 15 16 17 18 19 20 25])
 ;; ---------- Subscriptions ----------
 (rf/reg-sub
   ::player-1-name
@@ -20,9 +21,18 @@
     (get-in db [:game-state player :score score])))
 
 (rf/reg-sub
+  ::added-score
+  (fn [db [_ player]]
+    (get-in db [:game-state player :added-score])))
+
+(rf/reg-sub
   ::end-game?
   (fn [db [_]]
-    (get-in db [:game-state :end-game?])))
+    (->> NUMBERS
+         (map (fn [number]
+                (and (<= 3 (get-in db [:game-state :player-1 :score number]))
+                     (<= 3 (get-in db [:game-state :player-2 :score number])))))
+         (every? true?))))
 
 (rf/reg-sub
   ::total-score
@@ -39,11 +49,11 @@
   (fn [db [_ score]]
     (let [score-1 (get-in db [:game-state :player-1 :score score])
           score-2 (get-in db [:game-state :player-2 :score score])]
-     (cond
-       (and (<= 3 score-1) (<= 3 score-2)) :closed
-       (<= 3 score-1) :player-1
-       (<= 3 score-2) :player-2
-       :else :none))))
+      (cond
+        (and (<= 3 score-1) (<= 3 score-2)) :closed
+        (<= 3 score-1) :player-1
+        (<= 3 score-2) :player-2
+        :else :none))))
 
 ;; ---------- Events ----------
 (rf/reg-event-db
@@ -65,83 +75,117 @@
     (assoc-in db [:game-state :player-2 :name] val)))
 
 (rf/reg-event-db
+  ::set-added-score
+  (fn [db [_ player val]]
+    (assoc-in db [:game-state player :added-score] val)))
+
+(rf/reg-event-db
   ::cap-name
   (fn [db [_ player]]
     (let [name (get-in db [:game-state player :name])]
       (assoc-in db [:game-state player :name] (str/capitalize name)))))
 
-(rf/reg-event-db
+(rf/reg-event-fx
   ::add-player-score
-  (fn [db [_ player score amount]]
+  (fn [{:keys [db]} [_ player score]]
     (let [score-1 (get-in db [:game-state :player-1 :score score])
           score-2 (get-in db [:game-state :player-2 :score score])
           current-amount (get-in db [:game-state player :score score])]
       (cond
         (and (<= 3 score-1) (<= 3 score-2)) db
-        (and (= player :player-1) (<= 3 score-2)) (-> db
-                                                      (update :old-states (fn [e] (take 20 (conj e (get db :game-state)))))
-                                                      (assoc-in [:game-state player :score score] (min 3 (+ current-amount amount))))
-        (and (= player :player-2) (<= 3 score-1)) (-> db
-                                                      (update :old-states (fn [e] (take 20 (conj e (get db :game-state)))))
-                                                      (assoc-in [:game-state player :score score] (min 3 (+ current-amount amount))))
-        :else (-> db
-                  (update :old-states (fn [e] (take 20 (conj e (get db :game-state)))))
-                  (assoc-in [:game-state player :score score] (+ current-amount amount)))))))
+        (and (= player :player-1) (<= 3 score-2)) {:db (-> db
+                                                       (update :old-states (fn [e] (take 20 (conj e (get db :game-state)))))
+                                                       (assoc-in [:game-state player :score score] (min 3 (+ current-amount 1))))}
+        (and (= player :player-2) (<= 3 score-1)) {:db (-> db
+                                                       (update :old-states (fn [e] (take 20 (conj e (get db :game-state)))))
+                                                       (assoc-in [:game-state player :score score] (min 3 (+ current-amount 1))))}
+        :else {:db (-> db
+                   (update :old-states (fn [e] (take 20 (conj e (get db :game-state)))))
+                   (assoc-in [:game-state player :score score] (+ current-amount 1))
+                   (assoc-in [:game-state player :added-score] true)
+                   (assoc-in [:game-state player :added-score] true))
+               :dispatch-later {:ms 300 :dispatch [::set-added-score player false]}}))))
 
+(rf/reg-event-db
+  ::reset-game
+  (fn [db [ one two three]]
+    (js/console.log db one two three)
+    (-> db
+        (dissoc :old-states)
+        (update-in [:game-state :player-1] dissoc :score)
+        (update-in [:game-state :player-2] dissoc :score))))
 ;; ---------- Helpers -----------
 
 
 ;; ---------- Views -----------
 (defn winner-screen []
-  (let [end-game? @(rf/subscribe [::end-game?])]
+  (let [end-game? @(rf/subscribe [::end-game?])
+        player-1-name (or @(rf/subscribe [::player-1-name]) "Player 1")
+        player-1-score @(rf/subscribe [::total-score :player-1])
+        player-2-name (or @(rf/subscribe [::player-2-name]) "Player 2")
+        player-2-score @(rf/subscribe [::total-score :player-2])]
     (when end-game?
-     [:div.absolute.flex.justify-center.items-center.bg-opacity-80.bg-gray-500.w-full
-      {:style {:height  "95%"
-               :z-index 100}}
-      [:div.flex.flex-col.items-center.bg-white.w-1_2.h-1_2
-       [:div.flex.w-full.justify-end [:div [:i.fas.fa-times]]]
-       [:div.font-bold.text-3xl "The Winner is:"]
-       [:div.font-bold.text-3xl "FIX ME"]
-       [:div.flex.w-1_2.justify-evenly [:button "Restart"] [:button "Close"]]]])))
+      [:div.absolute.flex.justify-center.items-center.bg-opacity-80.bg-gray-500.w-full
+       {:style {:height  "95%"
+                :z-index 100}}
+       [:div.flex.flex-col.items-center.bg-white.w-1_2.h-1_2.p-4.rounded-lg.text-4xl
+        [:div.font-semibold "The Winner is:"]
+        [:div.font-bold
+         (cond
+           (= player-1-score player-2-score)
+           "Both Players!"
+           (< player-1-score player-2-score)
+           player-2-name
+           :else
+           player-1-name)]
+        [:div.pt-10 "Final Scores:"]
+        [:div.grid.gap-4 {:style {:grid-template-columns "1fr 1fr"}}
+         [:div player-1-name] [:div.text-right player-1-score]
+         [:div player-2-name] [:div.text-right player-2-score]]
+        [:div.flex.flex-grow]
+        [:div.flex.w-1_2.justify-center.items-end
+         [:button.p-4.border.border-gray-400.rounded.hover:bg-blue-200
+          {:on-click #(rf/dispatch [::reset-game])}
+          "Restart"]]]])))
 
+(defn total-line []
+  [:<>
+   [:div#score1.flex-center.text-4xl
+    {:class (when @(rf/subscribe [::added-score :player-2]) "animate-bounce-once")}
+    @(rf/subscribe [::total-score :player-1])]
+   [:div.flex-center.text-4xl.col-span-2.h-20 "Total"]
+   [:div#score2.flex-center.text-4xl
+    {:class (when @(rf/subscribe [::added-score :player-2]) "animate-bounce-once")}
+    @(rf/subscribe [::total-score :player-2])]])
 
 (defn grid-column [score]
   (let [player-1-score @(rf/subscribe [::player-score :player-1 score])
         player-2-score @(rf/subscribe [::player-score :player-2 score])]
-   [:<>
-    [:div.flex-center.text-2xl (if (< 3 player-1-score)
-                     (* score (- player-1-score 3))
-                     "0")]
-    [:div.flex.justify-center
-     [:button.cricket-btn {:on-click #(rf/dispatch [::add-player-score :player-1 score 3])} "+3"]
-     [:button.cricket-btn {:on-click #(rf/dispatch [::add-player-score :player-1 score 2])} "+2"]
-     [:button.cricket-btn {:on-click #(rf/dispatch [::add-player-score :player-1 score 1])} "+1"]]
-    [:div.flex-center
-     [:div.h-full.w-2.border.border-black.m-1 {:class (when (>= player-1-score 1) "bg-red-500")}]
-     [:div.h-full.w-2.border.border-black.m-1 {:class (when (>= player-1-score 2) "bg-red-500")}]
-     [:div.h-full.w-2.border.border-black.m-1 {:class (when (>= player-1-score 3) "bg-red-500")}]]
-    [:div.flex-center.text-4xl
-     {:class (case @(rf/subscribe [::number-open? score])
-               :player-1
-               "border-8 border-red-700"
-               :player-2
-               "border-8 border-blue-700"
-               :closed
-               "line-through"
-               ;else
-               "")}
-     (if (= score 25) "Bullseye" score)]
-    [:div.flex-center
-     [:div.h-full.w-2.border.border-black.m-1 {:class (when (>= player-2-score 1) "bg-blue-500")}]
-     [:div.h-full.w-2.border.border-black.m-1 {:class (when (>= player-2-score 2) "bg-blue-500")}]
-     [:div.h-full.w-2.border.border-black.m-1 {:class (when (>= player-2-score 3) "bg-blue-500")}]]
-    [:div.flex-center
-     [:button.cricket-btn {:on-click #(rf/dispatch [::add-player-score :player-2 score 1])} "+1"]
-     [:button.cricket-btn {:on-click #(rf/dispatch [::add-player-score :player-2 score 2])} "+2"]
-     [:button.cricket-btn {:on-click #(rf/dispatch [::add-player-score :player-2 score 3])} "+3"]]
-    [:div.flex-center.text-2xl (if (< 3 player-2-score)
-                     (* score (- player-2-score 3))
-                     "0")]]))
+    [:<>
+     [:div.flex.w-full.justify-end
+      [:div.flex-center.w-full.h-20.rounded.hover:bg-blue-300.cursor-pointer.border.border-gray-300
+       {:on-click #(rf/dispatch [::add-player-score :player-1 score 1])}
+       (when (>= 0 player-1-score) [:div.absolute.text-3xl.text-opacity-20.text-black.select-none "+1"])
+       (when (>= player-1-score 1) [:i.absolute.fa.fa-slash])
+       (when (>= player-1-score 2) [:i.absolute.fa.fa-slash.fa-flip-horizontal])
+       (when (>= player-1-score 3) [:i.absolute.far.fa-circle])]]
+     [:div.flex-center.text-4xl.col-span-2
+      {:class (case @(rf/subscribe [::number-open? score])
+                :player-1
+                "border-l-8 border-red-700"
+                :player-2
+                "border-r-8 border-blue-700"
+                :closed
+                "line-through"
+                ;else
+                "")}
+      (if (= score 25) "Bullseye" score)]
+     [:div.flex-center.w-full.h-20.rounded.hover:bg-blue-300.cursor-pointer.border.border-gray-300
+      {:on-click #(rf/dispatch [::add-player-score :player-2 score 1])}
+      (when (>= 0 player-2-score) [:div.absolute.text-3xl.text-opacity-20.text-black.select-none "+1"])
+      (when (>= player-2-score 1) [:i.absolute.fa.fa-slash])
+      (when (>= player-2-score 2) [:i.absolute.fa.fa-slash.fa-flip-horizontal])
+      (when (>= player-2-score 3) [:i.absolute.far.fa-circle])]]))
 
 (defn cricket []
   [:div.flex.justify-center.w-full.overflow-auto
@@ -151,26 +195,22 @@
     [:div.absolute.top-3.right-2.flex-center.border.border-black.rounded-full.px-2.hover:bg-blue-200
      [:button.pr-2 {:on-click #(rf/dispatch [::undo])} "Undo"]
      [:i.fa-solid.fa-rotate-left.fa-sm]]
-    [:div.grid.gap-4.text-2xl {:style {:grid-template-columns "1fr 2fr 1fr 2fr 1fr 2fr 1fr"}}
-     [:div.flex.justify-center @(rf/subscribe [::total-score :player-1])]
+    [:div.grid.gap-4.text-2xl {:style {:grid-template-columns "1fr 1fr 1fr 1fr"}}
      [:input.col-span-2.bg-transparent.text-center.border-b-2.border-red-500
       {:type        "text"
        :placeholder "Player 1"
        :value       @(rf/subscribe [::player-1-name])
        :on-change   #(rf/dispatch [::set-player-1-name (-> % .-target .-value)])
        :on-blur     #(rf/dispatch [::cap-name :player-1])}]
-     [:div]
      [:input.col-span-2.bg-transparent.text-center.border-b-2.border-blue-500
       {:type        "text"
        :placeholder "Player 2"
        :value       @(rf/subscribe [::player-2-name])
        :on-change   #(rf/dispatch [::set-player-2-name (-> % .-target .-value)])
        :on-blur     #(rf/dispatch [::cap-name :player-2])}]
-     [:div.flex.justify-center @(rf/subscribe [::total-score :player-2])]
 
      (doall
-       (for [score (range 15 21)]
+       (for [score NUMBERS]
          ^{:key score}
          [grid-column score]))
-
-     [grid-column 25]]]])
+     [total-line]]]])
